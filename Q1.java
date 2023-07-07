06a204981c5e3524fec7704faf36ea760854133d
su25100 <surbhibanke2599@gmail.com>
2023-07-07 20:07:23 +0530

    first commit q1

---
+++ Q1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Q1{
+
+    public static void main (String[]args){
+       int a,b;
+       int sum;
+       Scanner sc=new Scanner(System.in);
+       System.out.print("enter no1=");
+       a=sc.nextInt();
+       System.out.print("enter no2=");
+       b=sc.nextInt();
+
+       sum=a+b;
+       System.out.println("sum="+sum);
+
+       
+    }
+}
