1bf93d3f144708be0ad59d6e60641290d7cf55a5
su25100 <surbhibanke2599@gmail.com>
2023-07-12 04:02:43 +0530

    problem on string

---
+++ Q20.java
@@ -0,0 +1,21 @@
+//program to find ascii value of enter char
+import java.util.*;
+
+public class Q20 {
+    
+public static void main(String[]args){
+   
+    char c='a';
+    int a=c;
+
+System.out.print("ascii:"+a);
+    
+
+
+
+
+}
+
+
+
+}
